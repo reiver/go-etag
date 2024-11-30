6d2ea1c4f52113748b35c34c7efcc2d6d4dfe957
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-11-30 03:51:31 -0800

    http error

---
+++ httperror.go
@@ -0,0 +1,10 @@
+package etag
+
+import (
+        "net/http"
+)
+
+// httpError deals with writing a basic HTTP response error.
+func httpError(responseWriter http.ResponseWriter, code int) {
+	http.Error(responseWriter, http.StatusText(code), code)
+}
